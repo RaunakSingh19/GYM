6dcb93a54be2073d0a885fd66071a767eca72700
“raunaksingh19” <raunaksingh142004@gmail.com>
2025-05-15 05:32:33 -0700

    pusing config file

---
+++ backend/config/db.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log('MongoDB connected');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
+// module.exports = {
+//   mongoURI: 'mongodb+srv://user2000:test123@cluster0.0bw3o.mongodb.net/'
+// };
